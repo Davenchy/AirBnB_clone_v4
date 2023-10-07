7449a2a9c913051f86c881e5461738db8e67eb16
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-10-07 12:38:06 +0000

    make a progress

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -5,4 +5,13 @@ window.onload = function () {
   //   if the checkbox is checked, you must store the Amenity ID in a variable (dictionary or list)
   //   if the checkbox is unchecked, you must remove the Amenity ID from the variable
   //   update the h4 tag inside the div Amenities with the list of Amenities checked
+  let checkedAmenityIDs = []
+  $('input').on('change', function () {
+    if (this.checked) {
+      checkedAmenityIDs.push($(this).attr('data-id'));
+    } else {
+      checkedAmenityIDs.pop($(this).attr('data-id'));
+    }
+    console.log(checkedAmenityIDs);
+  });
 };
