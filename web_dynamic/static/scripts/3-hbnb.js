c659064b6023963c655721eadde3750ead4f8bf6
Fadi Asaad <firon1222@gmail.com>
2023-10-08 05:44:19 +0300

    implement js code to control the state of the server up status indicator

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,8 +1,8 @@
-window.onload = function () {
+window.onload = function() {
   const $ = window.$;
   const checkedAmenityIDs = [];
   const checkedAmenityNames = [];
-  $("input").on("change", function () {
+  $("input").on("change", function() {
     if (this.checked) {
       checkedAmenityIDs.push($(this).attr("data-id"));
       checkedAmenityNames.push($(this).attr("data-name"));
@@ -13,11 +13,28 @@ window.onload = function () {
     $("div.amenities h4").text(checkedAmenityNames.join(", "));
   });
 
+  $.ajax('http://0.0.0.0:5001/api/v1/status/', {
+    success: function(data, status) {
+      if (status === 'success') {
+        if (data.status === 'OK') {
+          $('DIV#api_status').addClass('available');
+        }
+      } else {
+        $('DIV#api_status').removeClass('available');
+      }
+    },
+    error: function(textStatus, errorThrown) {
+      $('DIV#api_status').removeClass('available');
+      console.log(textStatus);
+      console.log(errorThrown);
+    }
+  });
+
   $.ajax("http://0.0.0.0:5001/api/v1/places_search/", {
     type: "POST",
     data: "{}",
     contentType: "application/json",
-    success: function (data) {
+    success: function(data) {
       console.log(data);
       for (const place of data) {
         $("section.places").append(`<article>
--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -38,6 +38,23 @@ window.onload = function() {
     },
   });
 
+  $.ajax('http://0.0.0.0:5001/api/v1/status/', {
+    success: function (data, status) {
+      if (status === 'success') {
+        if (data.status === 'OK') {
+          $('DIV#api_status').addClass('available');
+        }
+      } else {
+        $('DIV#api_status').removeClass('available');
+      }
+    },
+    error: function (textStatus, errorThrown) {
+      $('DIV#api_status').removeClass('available');
+      console.log(textStatus);
+      console.log(errorThrown);
+    }
+  });
+
   $("button").click(function() {
     $.ajax("http://0.0.0.0:5001/api/v1/places_search/", {
       type: "POST",
